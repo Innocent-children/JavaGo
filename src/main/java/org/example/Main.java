bbe67520257d00782abb4a32da5de14f140c407f
Innocent-children <55626758+Innocent-children@users.noreply.github.com>
2022-08-29 08:24:36 +0800

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world\u2122!");
+        IntStream intStream = "abc".codePoints();
+        int[] array = intStream.toArray();
+        for (int i :
+                array) {
+            System.out.println(i);
+        }
     }
 }
